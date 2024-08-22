164e16de3caf08378d85a03e47f1eefa3e193178
Dudu <echavezv@utea.edu.pe>
2024-08-21 20:55:10 -0500

    Verificacion de elemento con atributos

--- app/HtmlElement.php
+++ app/HtmlElement.php
@@ -4,10 +4,39 @@ namespace App;
 
 class HtmlElement
 {
-  public function render() {
+    private $name;
+    private $content;
+    private $attributes;
 
-    return "ETIQUETAS HTML";
-    
- }
+    public function __construct(string $name, array $attributes = [], $content = null)
+    {
+        $this->name = $name;
+        $this->content = $content;
+        $this->attributes = $attributes;
+    }
+
+    public function render()
+    {
+        //Si el elemento tiene atributos
+        if (!empty($this->attributes)) {
+
+            $htmlAttributes = '';
+            foreach ($this->attributes as $name => $value) {
+                $htmlAttributes .= $name . '="' . $value.'"'; //name="value"
+            }
+            //Abrir la etiqueta con atributos
+            $result = '<' . $this->name .' '. $htmlAttributes . '>';
+        } else {
+
+            //Abrir la etiqueta sin atributos
+            $result = '<' . $this->name . '>';
+        }
+
+        //Imprimir el contenido
+        $result .= $this->content;
+        //Cerrar la etiqueta 
+        $result .= '</' . $this->name . '>';
+
+        return $result;
+    }
 }
-
--- public/index.php
+++ public/index.php
@@ -2,6 +2,10 @@
 
 require '../vendor/autoload.php';
 
-$element = new \App\HtmlElement;
+$element = new \App\HtmlElement('p',['id' => 'my_paragraph'],'Este es el contenido');
+
+echo $element->render();
+
+$element = new \App\HtmlElement('p',[],'Este es el contenido 2');
 
 echo $element->render();
